d77ac41340ba0d609cc83676ba61a5154e815fdf
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-23 10:25:16 +0200

    [FEAT] Add initial project
    Added starter spring web project with required dependencies

---
+++ src/main/java/com/github/unovajournal/UnovaJournalApplication.java
@@ -0,0 +1,13 @@
+package com.github.unovajournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnovaJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnovaJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/unovajournal/UnovaJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.unovajournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnovaJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
